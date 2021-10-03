8b0b4d34d8a5840c31575ba299d0cfdf61b8b484
Amin <amin.yaraghi@gmail.com>
2021-10-03 15:18:29 +0200

    Correct MEDIA_ROOT in urls.py file and correct import of static module

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.conf import settings
 
 
@@ -26,5 +26,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
-        document_root=settings.MEDIA_URL
+        document_root=settings.MEDIA_ROOT,
     )
